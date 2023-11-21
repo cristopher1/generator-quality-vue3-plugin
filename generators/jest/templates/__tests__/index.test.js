fb6dd645f3b8a5428ebe686ac70d7a1a9c904014
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-20 23:21:46 -0300

    test(jest): se hace mock de console.log y se agrega test para la global property

--- generators/jest/templates/__tests__/index.test.js
+++ generators/jest/templates/__tests__/index.test.js
@@ -1,24 +1,40 @@
-import { beforeAll } from '@jest/globals'
-import { createApp } from './helpers'
+import { jest, beforeEach } from '@jest/globals'
+import { faker, createApp } from './helpers'
 import { createInstaller } from '../src/main.js'
+
+jest.spyOn(console, 'log').mockImplementation((msg) => msg)
 
 const filePath = 'src/main.js'
 
 describe(`export function createInstaller (${filePath})`, () => {
-  const app = createApp()
-  const getGreeting = () => `hello from test`
+  let app
 
-  beforeAll(() => {
-    app.install(createInstaller(), { getGreeting })
+  beforeEach(() => {
+    app = createApp()
   })
-
   it('Should work the $getGreeting global property', () => {
     // Arrange
+    const getGreeting = () => 'hello from test'
+    app.install(createInstaller(), { getGreeting })
+
     const { $getGreeting } = app.config.globalProperties
     const expected = getGreeting()
 
     // Act
     const result = $getGreeting()
+
+    // Assert
+    expect(result).toBe(expected)
+  })
+  it('Should work the $greet global property', () => {
+    // Arrange
+    app.install(createInstaller())
+
+    const { $greet } = app.config.globalProperties
+    const expected = faker.string.sample()
+
+    // Act
+    const result = $greet(expected)
 
     // Assert
     expect(result).toBe(expected)
