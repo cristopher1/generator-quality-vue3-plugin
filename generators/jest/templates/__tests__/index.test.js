88d3f98cafdf69b035202b80ceee914c5348dda0
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-20 23:45:45 -0300

    test(jest): se actualizan los tests realizados al plugin

--- generators/jest/templates/__tests__/index.test.js
+++ generators/jest/templates/__tests__/index.test.js
@@ -1,42 +1,55 @@
 import { jest, beforeEach } from '@jest/globals'
 import { faker, createApp } from './helpers'
 import { createInstaller } from '../src/main.js'
+import { Installer } from '../src/installer'
 
 jest.spyOn(console, 'log').mockImplementation((msg) => msg)
 
 const filePath = 'src/main.js'
 
 describe(`export function createInstaller (${filePath})`, () => {
-  let app
-
-  beforeEach(() => {
-    app = createApp()
-  })
-  it('Should work the $getGreeting global property', () => {
+  it('Should create the installer object', () => {
     // Arrange
-    const getGreeting = () => 'hello from test'
-    app.install(createInstaller(), { getGreeting })
-
-    const { $getGreeting } = app.config.globalProperties
-    const expected = getGreeting()
+    const expected = Installer
 
     // Act
-    const result = $getGreeting()
+    const result = createInstaller()
 
     // Assert
-    expect(result).toBe(expected)
+    expect(result).toBeInstanceOf(expected)
   })
-  it('Should work the $greet global property', () => {
-    // Arrange
-    app.install(createInstaller())
+  describe('Installer object', () => {
+    let app
 
-    const { $greet } = app.config.globalProperties
-    const expected = faker.string.sample()
+    beforeEach(() => {
+      app = createApp()
+    })
+    it('Should add the $getGreeting global property', () => {
+      // Arrange
+      const getGreeting = () => 'hello from test'
+      app.install(createInstaller(), { getGreeting })
 
-    // Act
-    const result = $greet(expected)
+      const { $getGreeting } = app.config.globalProperties
+      const expected = getGreeting()
 
-    // Assert
-    expect(result).toBe(expected)
+      // Act
+      const result = $getGreeting()
+
+      // Assert
+      expect(result).toBe(expected)
+    })
+    it('Should add the $greet global property', () => {
+      // Arrange
+      app.install(createInstaller())
+
+      const { $greet } = app.config.globalProperties
+      const expected = faker.string.sample()
+
+      // Act
+      const result = $greet(expected)
+
+      // Assert
+      expect(result).toBe(expected)
+    })
   })
 })
